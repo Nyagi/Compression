51bd4df89542c473ae6955a479c9ae215041fd72
Nyagi <33533469+Nyagi@users.noreply.github.com>
2022-08-02 00:52:14 -0400

    Update recipes.js

--- kubejs/startup_scripts/recipes.js
+++ kubejs/startup_scripts/recipes.js
@@ -178,5 +178,31 @@ onEvent('recipes', event => {
         A: 'kubejs:slightly_unstable_mix_bag',
         B: 'kubejs:sturdy_wooden_box'
       })
-  })
+    //Create Crafting
+      //Bonk (Press)
+      event.recipes.createPressing('kubejs:used_crude_compaction_testing_kit',['kubejs:crude_compaction_testing_kit'])
+    //Furnace Crafting
+      event.smelting('kubejs:crude_entropy_testing_kit', 'kubejs:used_crude_entropy_testing_kit')
+    //Create Assembly Line
+      //Static-Test
+      /*
+      event.recipes.createSequencedAssembly([ // start the recipe
+        Item.of('6x create:large_cogwheel').withChance(32.0), // have this item be an output with a certain chance
+        Item.of('create:brass_ingot').withChance(2.0), // have this item be an output with a certain chance
+            'minecraft:andesite', // have this item be a guaranteed output
+            'create:cogwheel', // have this item be a guaranteed output
+            'minecraft:stick', // have this item be a guaranteed output
+            'minecraft:iron_nugget' // have this item be a guaranteed output
+            ], 'kubejs:crude_entropy_testing_kit', [ // 'create:brass_ingot' is the input.
+            // the transitional item set by "transitionalItem('create:incomplete_large_cogwheel')" is the item that will be used during the recipe as the item that the input is using to transition to the output.
+            event.recipes.createDeploying
+                ('create:incomplete_large_cogwheel', ['create:incomplete_large_cogwheel', '#minecraft:planks']), // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
+            event.recipes.createDeploying
+                ('create:incomplete_large_cogwheel', ['create:incomplete_large_cogwheel', '#minecraft:wooden_buttons']), // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
+            event.recipes.createCutting
+                ('create:incomplete_large_cogwheel', 'create:incomplete_large_cogwheel').processingTime(50) // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
+]).transitionalItem('create:incomplete_large_cogwheel').loops(4) // set the transitional item and the loops (amount of repetitions)
+*/   
+}) 
+
 
