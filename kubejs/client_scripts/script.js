73186832f964335e558cf0a49e072ba9ae9d1bb5
Tralomine <selene.colin@protonmail.com>
2022-08-14 04:05:39 +0200

    tooltip and translations

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -6,3 +6,7 @@ onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
 })
+
+onEvent('item.tooltip', tooltip => {
+	tooltip.add(['botania:pebble'], 'I can hear some scuttling within blocks of stone, I have to be a tad cautious to not break it with a tool')
+})
--- kubejs/server_scripts/spawn_stone.js
+++ kubejs/server_scripts/spawn_stone.js
@@ -3,19 +3,17 @@ onEvent('block.break', event => {
 	let block = event.getBlock();
 	
 	if (player.getMainHandItem().isEmpty()) {
-		event.server.runCommand('/tellraw @a {"text":"'+block.id+'", "color":"blue"}')
 		if (block.id.startsWith('minecraft:stone')) {
 			let stone_flies = Math.floor(Math.random()*3+1);
 			for (let i = 0; i < stone_flies; i++) {
-				event.server.runCommand('/summon crittersandcompanions:dragonfly '+block.x+' '+block.y+' '+block.z+'')
+				event.server.runCommandSilent('/summon crittersandcompanions:dragonfly '+block.x+' '+block.y+' '+block.z+'')
 			}
 		}
 		let tags = block.getTags()
 		tags.forEach(t => {
-			event.server.runCommand('/tellraw @a {"text":"tag: '+t+'", "color":"green"}')
 			if (t.toString().equals("minecraft:logs")) {
-				event.server.runCommand('/summon crittersandcompanions:leaf_insect '+block.x+' '+block.y+' '+block.z+'')
-				event.server.runCommand('/setblock '+block.x+' '+block.y+' '+block.z+' air')
+				event.server.runCommandSilent('/summon crittersandcompanions:leaf_insect '+block.x+' '+block.y+' '+block.z+'')
+				event.server.runCommandSilent('/setblock '+block.x+' '+block.y+' '+block.z+' air')
 				event.cancel();
 			}
 		})
